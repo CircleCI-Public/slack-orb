2fd5834570c58d9e045ecaacef49095d78afe6b9
Jonathan Drake <jdrake@narrativescience.com>
2019-08-15 08:46:47 -0700

    0.1.0

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -4,5 +4,5 @@ set -e
 
 circleci config pack src > orb.yml
 circleci orb validate orb.yml
-circleci orb publish orb.yml narrativescience/slack@dev:alpha5
+circleci orb publish orb.yml narrativescience/slack@0.1.0
 rm -rf orb.yml
