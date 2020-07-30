c55343584d90180eefaac922b8e43050ee4ae369
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-07-30 12:05:57 -0400

    remove old tests. refactor notify script

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -1,9 +1,14 @@
 echo "Sending Notification"
 
-if [[ "$CCI_STATUS" == "<<parameters.event>>" || "<<parameters.event>>" == "always" ]]; then
-    # send message
-    # If sending message, default to custom template, if none is supplied, check for a pre-selected template value. If none, error.
+SetEnvVars() {
+    INTRNL_SLACK_WEBHOOK=$(eval echo "$SLACK_PARAM_WEBHOOK")
+}
 
+PostToSlack() {
+    # Send message
+    #   If sending message, default to custom template,
+    #   if none is supplied, check for a pre-selected template value.
+    #   If none, error.
     if [ -n "$SLACK_PARAM_CUSTOM" ]; then
         SLACK_MSG_BODY="$SLACK_PARAM_CUSTOM"
     elif [ -n "$SLACK_PARAM_TEMPLATE" ]; then
@@ -11,17 +16,30 @@ if [[ "$CCI_STATUS" == "<<parameters.event>>" || "<<parameters.event>>" == "alwa
     else
         echo "Error: No message template selected."
         echo "Select either a custom template or one of the pre-included ones via the 'custom' or 'template' parameters."
-        exit 0
+        exit 1
     fi
 
     curl -X POST -H 'Content-type: application/json' \
         --data \
-        "$SLACK_MSG_BODY" "<<parameters.webhook>>"
-else
-    # dont send message.
-    echo "NO SLACK ALERT"
-    echo
-    echo "This command is set to send an alert on: <<parameters.event>>"
-    echo "Current status: $CCI_STATUS"
-    exit 0
+        "$SLACK_MSG_BODY" "$INTRNL_SLACK_WEBHOOK"
+}
+
+Notify() {
+    SetEnvVars
+    if [[ "$CCI_STATUS" == "$SLACK_PARAM_EVENT" || "$SLACK_PARAM_EVENT" == "always" ]]; then
+    PostToSlack
+    else
+        # dont send message.
+        echo "NO SLACK ALERT"
+        echo
+        echo "This command is set to send an alert on: $SLACK_PARAM_EVENT"
+        echo "Current status: $CCI_STATUS"
+
+    fi
+}
+
+# Will not run if sourced from another script.
+# This is done so this script may be tested.
+if [[ "$_" == "$0" ]]; then
+    Notify
 fi
