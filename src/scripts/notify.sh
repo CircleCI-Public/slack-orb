15afc9b4660593878d04af1457cfab9125473ed6
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-10-05 16:01:16 -0400

    improved install script

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -42,8 +42,8 @@ PostToSlack() {
 Notify() {
     if [ "$CCI_STATUS" = "$SLACK_PARAM_EVENT" ] || [ "$SLACK_PARAM_EVENT" = "always" ]; then
     BranchFilter # In the event the Slack notification would be sent, first ensure it is allowed to trigger on this branch.
-    PostToSlack
     echo "Sending Notification"
+    PostToSlack
     else
         # dont send message.
         echo "NO SLACK ALERT"
@@ -66,20 +66,20 @@ ModifyCustomTemplate() {
 
 InstallJq() {
     if uname -a | grep Darwin > /dev/null 2>&1; then
-        echo "Installing JQ for Mac."
+        echo "Checking For JQ + CURL: MacOS"
         command -v jq >/dev/null 2>&1 || HOMEBREW_NO_AUTO_UPDATE=1 brew install jq --quiet
         return $?
 
     elif cat /etc/issue | grep Debian > /dev/null 2>&1 || cat /etc/issue | grep Ubuntu > /dev/null 2>&1; then
+        echo "Checking For JQ + CURL: Debian"
         if [ "$(id -u)" = 0 ]; then export SUDO=""; else # Check if we're root
             export SUDO="sudo";
         fi
-        echo "Installing JQ for Debian."
-        $SUDO apt -qq update
-        $SUDO apt -qq install -y jq
+        command -v jq >/dev/null 2>&1 || { $SUDO apt -qq update && $SUDO apt -qq install -y jq; }
         return $?
 
     elif cat /etc/issue | grep Alpine > /dev/null 2>&1; then
+        echo "Checking For JQ + CURL: Alpine"
         command -v curl >/dev/null 2>&1 || { echo >&2 "SLACK ORB ERROR: CURL is required. Please install."; exit 1; }
         command -v jq >/dev/null 2>&1 || { echo >&2 "SLACK ORB ERROR: JQ is required. Please install"; exit 1; }
         return $?
