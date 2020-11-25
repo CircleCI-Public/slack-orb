1041c8bf6d3a764e3b33cf3cede629f820340198
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-11-25 09:03:59 +0000

    Fix order of operations with shouldpost.

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -135,9 +135,9 @@ ShouldPost() {
 # This is done so this script may be tested.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" = "$0" ]; then
-    ShouldPost
     CheckEnvVars
     . "/tmp/SLACK_JOB_STATUS"
+    ShouldPost
     InstallJq
     BuildMessageBody
     PostToSlack
