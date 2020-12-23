847535127d4b4c9c8deb2ff512ff1b7290bf168c
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-12-23 09:58:20 -0500

    Change parameter name to ignore_errors + readme edit

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -35,11 +35,12 @@ PostToSlack() {
         SLACK_SENT_RESPONSE=$(curl -s -f -X POST -H 'Content-type: application/json' -H "Authorization: Bearer $SLACK_ACCESS_TOKEN" --data "$SLACK_MSG_BODY" https://slack.com/api/chat.postMessage)
         SLACK_ERROR_MSG=$(echo "$SLACK_SENT_RESPONSE" | jq '.error')
         if [ ! "$SLACK_ERROR_MSG" = "null" ]; then
-            echo "There was an error sending the Slack message. Please view the error below."
-            echo "ERROR: $SLACK_ERROR_MSG"
+            echo "Slack API returned an error message:"
+            echo "$SLACK_ERROR_MSG"
+            echo
             echo
             echo "View the Setup Guide: https://github.com/CircleCI-Public/slack-orb/wiki/Setup"
-            if [ "$SLACK_PARAM_EXIT_ON_FAIL" = "1" ]; then
+            if [ "$SLACK_PARAM_IGNORE_ERRORS" = "0" ]; then
                 exit 1
             fi
         fi
