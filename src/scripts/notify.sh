b37f189364241efe360f7d44829593dedf13c8b5
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-01-31 07:32:17 -0800

    add echo message back

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -72,6 +72,7 @@ ModifyCustomTemplate() {
 }
 
 InstallJq() {
+    echo "Checking For JQ + CURL"
     if command -v curl >/dev/null 2>&1 && ! command -v jq >/dev/null 2>&1; then
         uname -a | grep Darwin > /dev/null 2>&1 && JQ_VERSION=jq-osx-amd64 || JQ_VERSION=jq-linux32
         curl -Ls -o $JQ_PATH https://github.com/stedolan/jq/releases/download/jq-1.6/${JQ_VERSION}
