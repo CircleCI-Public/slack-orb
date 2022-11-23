644a1af9c82e441011bf721adbeed287d7759599
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-11-23 12:32:00 -0800

    fix: shellcheck error

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -4,4 +4,4 @@
 printf '%s' "$SLACK_SCRIPT_NOTIFY" > "notify.sh"
 chmod +x "notify.sh"
 ./notify.sh
-rm -rf "notify.sh" || { printf '%s\n' "Could not clean the working directory." }
+rm -rf "notify.sh" || { printf '%s\n' "Could not clean the working directory."; }
