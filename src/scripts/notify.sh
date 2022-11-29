afb96bdc65cc9ab561de90232ce6f6e35b7869c3
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-11-29 07:35:29 -0800

    feat: add windows support (#380)
    * feat: implement workaround for windows support

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC3043
 
+# Import utils.
+eval "$SLACK_SCRIPT_UTILS"
 JQ_PATH=/usr/local/bin/jq
 
 BuildMessageBody() {
@@ -9,7 +11,7 @@ BuildMessageBody() {
     #   if none is supplied, check for a pre-selected template value.
     #   If none, error.
     if [ -n "${SLACK_PARAM_CUSTOM:-}" ]; then
-        SanitizeVars "$SLACK_PARAM_CUSTOM" 
+        SanitizeVars "$SLACK_PARAM_CUSTOM"
         ModifyCustomTemplate
         # shellcheck disable=SC2016
         CUSTOM_BODY_MODIFIED=$(echo "$CUSTOM_BODY_MODIFIED" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/`/\\`/g')
@@ -30,7 +32,7 @@ BuildMessageBody() {
         T1="$(printf '%s' "$template_body" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/`/\\`/g')"
         T2="$(eval printf '%s' \""$T1"\")"
     fi
-   
+
     # Insert the default channel. THIS IS TEMPORARY
     T2="$(printf '%s' "$T2" | jq ". + {\"channel\": \"$SLACK_DEFAULT_CHANNEL\"}")"
     SLACK_MSG_BODY="$T2"
@@ -181,20 +183,11 @@ SetupLogs() {
     fi
 }
 
-ExitIfWindows() {
-    os="$(uname -s | tr '[:upper:]' '[:lower:]')"
-    if printf '%s\n' "$os" | grep -q 'msys*\|cygwin*' ; then
-        printf '%s\n' "Windows is not supported by this orb."
-        printf '%s\n' "For more information, see: https://github.com/CircleCI-Public/slack-orb/wiki/FAQ."
-        exit 1
-    fi
-}
-
 # $1: Template with environment variables to be sanitized.
 SanitizeVars() {
   [ -z "$1" ] && { printf '%s\n' "Missing argument."; return 1; }
   local template="$1"
-  
+
   # Find all environment variables in the template with the format $VAR or ${VAR}.
   # The "|| true" is to prevent bats from failing when no matches are found.
   local variables
@@ -204,23 +197,30 @@ SanitizeVars() {
   # Extract the variable names from the matches.
   local variable_names
   variable_names="$(printf '%s\n' "$variables" | grep -o -E '[a-zA-Z0-9_]+')"
-  
+
+  # Find out what OS we're running on.
+  detect_os
+
   for var in $variable_names; do
     # The variable must be wrapped in double quotes before the evaluation.
     # Otherwise the newlines will be removed.
     local value
     value="$(eval printf '%s' \"\$"$var\"")"
     [ -z "$value" ] && { printf '%s\n' "$var is empty or doesn't exist. Skipping it..."; continue; }
-    
+
     printf '%s\n' "Sanitizing $var..."
- 
+
     local sanitized_value="$value"
     # Escape backslashes.
     sanitized_value="$(printf '%s' "$sanitized_value" | awk '{gsub(/\\/, "&\\"); print $0}')"
     # Escape newlines.
     sanitized_value="$(printf '%s' "$sanitized_value" | awk 'NR > 1 { printf("\\n") } { printf("%s", $0) }')"
     # Escape double quotes.
-    sanitized_value="$(printf '%s' "$sanitized_value" | awk '{gsub(/\"/, "\\\""); print $0}')"
+    if [ "$PLATFORM" = "windows" ]; then
+        sanitized_value="$(printf '%s' "$sanitized_value" | awk '{gsub(/"/, "\\\""); print $0}')"
+    else
+        sanitized_value="$(printf '%s' "$sanitized_value" | awk '{gsub(/\"/, "\\\""); print $0}')"
+    fi
 
     # Write the sanitized value back to the original variable.
     # shellcheck disable=SC3045 # This is working on Alpine.
@@ -234,7 +234,6 @@ SanitizeVars() {
 # This is done so this script may be tested.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
-    ExitIfWindows
     # shellcheck source=/dev/null
     . "/tmp/SLACK_JOB_STATUS"
     ShouldPost
