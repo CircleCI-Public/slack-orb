8ed572d08213517a43c6bfe5d57dbcb5ecf4abc0
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-07-15 14:37:00 -0500

    Add reaction command

---
+++ src/scripts/reaction.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+if [ "$SLACK_PARAM_DEBUG" = true ]; then
+    set -x
+fi
+
+SLACK_PARAM_CHANNEL="$(echo "${SLACK_PARAM_CHANNEL}" | circleci env subst)"
+
+ReactToSlack() {
+    # replace non-alpha
+    SLACK_PARAM_THREAD=$(echo "$SLACK_PARAM_THREAD" | sed -r 's/[^[:alpha:][:digit:].]/_/g')
+
+    if [ ! "$SLACK_PARAM_THREAD" = "" ] && [ -f "/tmp/SLACK_THREAD_INFO/$SLACK_PARAM_CHANNEL" ]; then
+        # shellcheck disable=SC2002
+        SLACK_THREAD_EXPORT=$(grep -m1 "$SLACK_PARAM_THREAD" /tmp/SLACK_THREAD_INFO/"${SLACK_PARAM_CHANNEL}" || [ "$?" = "1" ])
+        if [ ! "$SLACK_THREAD_EXPORT" = "" ]; then
+            # if there is an initial message with a thread id, load it into the environment
+            # thread_id=12345.12345
+            eval "$SLACK_THREAD_EXPORT"
+        fi
+        # get the value of the specified thread from the environment
+        # SLACK_THREAD_TS=12345.12345
+        SLACK_THREAD_TS=$(eval "echo \"\$$SLACK_PARAM_THREAD\"")
+    fi
+
+    if [ "${SLACK_PARAM_REMOVE_REACT_NAME}" != "" ]; then
+        echo "Remove reaction with name=${SLACK_PARAM_REMOVE_REACT_NAME} channel=${SLACK_PARAM_CHANNEL} thread_ts=${SLACK_THREAD_TS}"
+        curl --location 'https://slack.com/api/reactions.remove' \
+            --header 'Content-Type: application/x-www-form-urlencoded' \
+            --header "Authorization: Bearer ${SLACK_ACCESS_TOKEN}" \
+            --data-urlencode "channel=${SLACK_PARAM_CHANNEL}" \
+            --data-urlencode "name=${SLACK_PARAM_REMOVE_REACT_NAME}" \
+            --data-urlencode "timestamp=${SLACK_THREAD_TS}"
+        echo
+    fi
+    if [ "${SLACK_PARAM_REMOVE_REACT_NAME}" != "" ]; then
+        echo "Add reaction with name=${SLACK_PARAM_ADD_REACT_NAME} channel=${SLACK_PARAM_CHANNEL} thread_ts=${SLACK_THREAD_TS}"
+        curl --location 'https://slack.com/api/reactions.add' \
+            --header 'Content-Type: application/x-www-form-urlencoded' \
+            --header "Authorization: Bearer ${SLACK_ACCESS_TOKEN}" \
+            --data-urlencode "channel=${SLACK_PARAM_CHANNEL}" \
+            --data-urlencode "name=${SLACK_PARAM_ADD_REACT_NAME}" \
+            --data-urlencode "timestamp=${SLACK_THREAD_TS}"
+        echo
+    fi
+}
+
+ReactToSlack
+set +x
