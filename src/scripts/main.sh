b399515a1f9e5e5c2a93e29a03f4958fcc231c4f
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-08-26 09:44:56 -0500

    Add more debugg

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -3,4 +3,8 @@
 # Workaround for Windows Support
 # For details, see: https://github.com/CircleCI-Public/slack-orb/pull/380
 # shellcheck source=/dev/null
+if [ "$SLACK_PARAM_DEBUG" -eq 1 ]; then
+    set -x
+fi
+
 eval printf '%s' "$SLACK_SCRIPT_NOTIFY"
