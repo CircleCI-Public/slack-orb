32c70d7ef2842592ae10e74189b3a06e8e5c11f2
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-08-26 10:03:03 -0500

    Restore debug

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -3,6 +3,4 @@
 # Workaround for Windows Support
 # For details, see: https://github.com/CircleCI-Public/slack-orb/pull/380
 # shellcheck source=/dev/null
-set -x
-
 eval printf '%s' "$SLACK_SCRIPT_NOTIFY"
